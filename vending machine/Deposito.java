fe513935cf832a709491059dacc0bb80477b806a
ragnar707 <paolobertazzoli60@gmail.com>
2025-05-07 17:37:37 +0200

    Update Deposito.java

--- vending machine/Deposito.java
+++ vending machine/Deposito.java
@@ -27,31 +27,3 @@ public class Deposito {
     
 }
 =======
-public class Deposito {
-
-    private double totale;
-
-    public Deposito(){
-        this.totale = 0.0;
-    }
-
-    public void aggiungi(double importo){
-        totale += importo;
-    }
-
-    public double ritira(){
-        double prelievo = totale;
-        totale = 0.0;
-        return prelievo;
-    }
-
-    public double getTotale() {
-        return totale;
-    }
-
-    public void mostraDeposito(){
-        System.out.println("Totale nel deposito:" + totale);
-    }
-    
-}
->>>>>>> b64d03a9d08cb42ab95a05586a543d2dc79d4339
