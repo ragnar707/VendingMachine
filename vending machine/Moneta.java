d4c9a59d1196ac3ccd972c206e60a1d18bff358e
ragnar707 <paolobertazzoli60@gmail.com>
2025-05-07 17:39:50 +0200

    Update Moneta.java

--- vending machine/Moneta.java
+++ vending machine/Moneta.java
@@ -18,22 +18,3 @@ public class Moneta {
     }
 }
 =======
-public class Moneta {
-
-    private double valore;
-
-    public Moneta(double valore){
-        this.valore = valore;
-    }
-    
-    public double getValore() {
-        return valore;
-    }
-
-    @Override
-    public String toString() {
-        // TODO Auto-generated method stub
-        return String.format("2f", valore);
-    }
-}
->>>>>>> b64d03a9d08cb42ab95a05586a543d2dc79d4339
